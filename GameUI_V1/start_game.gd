cf598f7301c3937b078d2b9d5340322cc06eb017
JusteliZz <j.skiparius@gmail.com>
2025-03-11 21:48:49 +0200

    UI Implementation

---
+++ GameUI_V1/start_game.gd
@@ -0,0 +1,21 @@
+extends Control
+
+
+
+func _on_empty_game_1_pressed() -> void:
+	
+	get_tree().change_scene_to_file("res://level.tscn")
+
+
+func _on_empty_game_2_pressed() -> void:
+	
+	get_tree().change_scene_to_file("res://level.tscn")
+
+
+func _on_empty_game_3_pressed() -> void:
+	
+	get_tree().change_scene_to_file("res://level.tscn")
+
+
+func _on_back_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://GameUI_V1/MainMenu.tscn")
