56b0c093defc576446611c3f712eda64ab1c9771
Algimantas <125826902+AlgimantasVz@users.noreply.github.com>
2025-03-20 12:54:54 +0200

    added camera pickup

--- interaction/interact_ray.gd
+++ interaction/interact_ray.gd
@@ -1,6 +1,12 @@
 extends RayCast3D
 
 @onready var prompt = $Prompt
+@onready var hand = $"../hand"
+@onready var photoCamera = preload("res://interaction/camera.tscn")
+@onready var heldCamera = preload("res://interaction/camera_hd.tscn")
+
+var cameraToDrop
+var cameraToSpawn
 
 func _physics_process(_delta):
 	
@@ -13,5 +19,19 @@ func _physics_process(_delta):
 		if collider is Interactable:
 			prompt.text = collider.prompt_message
 			
+			cameraToSpawn = heldCamera.instantiate()
+			
+			if hand.get_child(0) != null:
+				cameraToDrop = photoCamera.instantiate()
+			
 			if Input.is_action_just_pressed("interact"):
-				collider.interact(owner)
+				if cameraToSpawn != null:
+					if hand.get_child(0) != null:
+						get_parent().add_child(cameraToDrop)
+						cameraToDrop.global_transform = hand.global_transform
+						cameraToDrop = true
+						hand.get_child(0).queue_free()
+					get_collider().queue_free()
+					hand.add_child(cameraToSpawn)
+					cameraToSpawn.rotation = hand.rotation
+					
