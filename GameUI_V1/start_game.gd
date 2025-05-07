f039b2684842546dad09d690a86bfc51035c3ebe
JusteliZz <j.skiparius@gmail.com>
2025-05-07 13:55:38 +0300

    User folder updated

--- GameUI_V1/start_game.gd
+++ GameUI_V1/start_game.gd
@@ -1,5 +1,16 @@
 extends Control
 
+func _ready() -> void:
+	# Delete all files inside the screenshots directory
+	var screenshots_dir := DirAccess.open("user://screenshots")
+	if screenshots_dir:
+		screenshots_dir.list_dir_begin()
+		var file_name := screenshots_dir.get_next()
+		while file_name != "":
+			if not screenshots_dir.current_is_dir():
+				screenshots_dir.remove(file_name)
+			file_name = screenshots_dir.get_next()
+		screenshots_dir.list_dir_end()
 
 func _on_empty_game_1_pressed() -> void:
 	
